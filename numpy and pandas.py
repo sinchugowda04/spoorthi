9ef8ba104761f903f0a6a8b9dd58156372054dea
sinchugowda04 <sinchu332@gmail.com>
2025-07-04 00:08:01 -0700

    Update numpy and pandas.py

--- numpy and pandas.py
+++ numpy and pandas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np 
-data+pd.DataFrame([[9,4,8,9],[8,10,7,6],[7,6,8,5]],columns=['Maths','English','Science','History'])
+data=pd.DataFrame([[9,4,8,9],[8,10,7,6],[7,6,8,5]],columns=['Maths','English','Science','History'])
 print(data.agg(['sum','min','max']))
 m=lambda x:x+10
 print(m(5))
